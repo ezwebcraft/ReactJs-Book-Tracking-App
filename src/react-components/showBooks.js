f7e1260b04dff203810a69ffdc9e7c2c2d01bef8
Elvin Ramirez <real8686@gmail.com>
2017-09-17 20:16:57 -0400

    feat: add bookToShelf and book file component

--- src/react-components/showBooks.js
+++ src/react-components/showBooks.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-
+import Book from './Books';
 
 class showBooks extends Component {
 
@@ -15,16 +15,16 @@ static propTypes = {
 
  	const {books, title, bookToShelf} = this.props;
 
-
  		return(
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
+                    	 {books.map((book) => (<Book key={book.id} book={book} bookToShelf={bookToShelf}/>))}
          			</ol>
         		  </div>
       			</div>
-      			)
+      	)
 
 	}
 
