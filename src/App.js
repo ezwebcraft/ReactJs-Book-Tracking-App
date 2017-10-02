703691a9fcb4c5cbc6792e01cdc9de1d826cd426
Elvin Ramirez <real8686@gmail.com>
2017-10-01 20:23:12 -0400

    fix: redo project

--- src/App.js
+++ src/App.js
@@ -3,43 +3,21 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 import {Route} from 'react-router-dom'
 
-//Components
-import showBooks from './react-components/showBooks.js'
 
 class BooksApp extends React.Component {
   state = {
     books: []
   }
 
-getBooks = () => {
- // get the state in the API
- BooksAPI.getAll().then(books => {
-      this.setState({books});
-    })
-}
 
-placeBookToShelf = (book, shelf) => {
-    BooksAPI.update(book, shelf).then(data => {
-      this.setState(status => ({
-        books: status.books.map(b => {
-          if (book.id === b.id) {
-            b.shelf = shelf;
-          }
-          return b;
-        })
-      }));
-    });
-  }
   render() {
     const {books} = this.state;
     return (
-      <div className="app">
-        <Route exact path='/' render={() => (<showBooks books={books} placeBookToShelf={this.placeBookToShelf} getBooks={this.getBooks} /> )} />
+      <div className='app'>
       
       </div>
-
     )
   }
 }
 
-export default BooksApp
+export default BooksApp;
