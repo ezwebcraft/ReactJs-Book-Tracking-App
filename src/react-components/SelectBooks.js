f68006ffb780c8ee00bf91dfec06a5362cdcb607
Elvin Ramirez <real8686@gmail.com>
2017-10-03 22:58:20 -0400

    feat: add changeShelf for prop

--- src/react-components/SelectBooks.js
+++ src/react-components/SelectBooks.js
@@ -3,26 +3,30 @@ import PropTypes from 'prop-types';
 
 class SelectBooks extends Component {
  static propTypes = {
-
+    book: PropTypes.object.isRequired,
+    updateShelf: PropTypes.func.isRequired
   }
 
+  changeShelf = (event) => {
+    event.preventDefault();
+    this.props.updateShelf(this.props.book, event.target.value);
+  }
 
-//note on adding handlers
-
- render() {
- 	return (
-	      <div className="book-shelf-changer">
-	        <select>
-	          <option value="none" disabled>Move to...</option>
-	          <option value="currentlyReading">Currently Reading</option>
-	          <option value="wantToRead">Want to Read</option>
-	          <option value="read">Read</option>
-	          <option value="none">None</option>
-	        </select>
-	      </div>
+  render() {
+    const {book} = this.props
+    return (
+      <div className='book-shelf-changer'>
+        <select value={book.shelf} onChange={(e) => this.changeShelf(e)}>
+          <option value='none' disabled>Move to...</option>
+          <option value='currentlyReading'>Currently Reading</option>
+          <option value='wantToRead'>Want to Read</option>
+          <option value='read'>Read</option>
+          <option value='none'>None</option>
+        </select>
+      </div>
     );
-
   }
+
 }
 
 export default SelectBooks;
